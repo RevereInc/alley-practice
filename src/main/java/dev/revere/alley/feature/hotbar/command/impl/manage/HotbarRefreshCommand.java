be6f44c092168abd8667cceedfaae9c4f4635575
Hamza <174505732+hammzar@users.noreply.github.com>
2025-11-02 17:11:39 +0100

    chore: profile state check in HotbarRefreshCommand when applying hotbar items

--- src/main/java/dev/revere/alley/feature/hotbar/command/impl/manage/HotbarRefreshCommand.java
+++ src/main/java/dev/revere/alley/feature/hotbar/command/impl/manage/HotbarRefreshCommand.java
@@ -2,11 +2,19 @@ package dev.revere.alley.feature.hotbar.command.impl.manage;
 
 import dev.revere.alley.common.text.CC;
 import dev.revere.alley.core.locale.internal.impl.message.GlobalMessagesLocaleImpl;
+import dev.revere.alley.core.profile.Profile;
+import dev.revere.alley.core.profile.ProfileService;
+import dev.revere.alley.core.profile.enums.ProfileState;
 import dev.revere.alley.feature.hotbar.HotbarService;
 import dev.revere.alley.library.command.BaseCommand;
 import dev.revere.alley.library.command.CommandArgs;
 import dev.revere.alley.library.command.annotation.CommandData;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author Hamza
@@ -33,7 +41,13 @@ public class HotbarRefreshCommand extends BaseCommand {
         }
 
         try {
-            plugin.getServer().getOnlinePlayers().forEach(hotbarService::applyHotbarItems);
+            plugin.getServer().getOnlinePlayers().forEach(player -> {
+                Profile profile = plugin.getService(ProfileService.class).getProfile(player.getUniqueId());
+                if (profile.isInGame()) {
+                    return;
+                }
+                hotbarService.applyHotbarItems(plugin.getServer().getPlayer(profile.getUuid()));
+            });
         } catch (Exception e) {
             sender.sendMessage(CC.translate("&cNo players are online to refresh hotbar items for."));
             return;
